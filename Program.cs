fd5bbb081f46d3e183035b05c2addf9f5fbc0367
SuuSuuPhyoe25 <suusuuphyoe25@gmail.com>
2026-01-03 17:12:11 +0800

    Added custom identity logic

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<CarRentalManagement1User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<CarRentalManagement1Context>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
